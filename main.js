bb6ed519cc9ed86e7a08bb116a4a1cb0138a2f8b
meskilsson <mattiaseskilsson@hotmail.se>
2025-09-24 10:38:17 +0200

    codewars workshop

---
+++ main.js
@@ -0,0 +1,34 @@
+//a. //MÅL:Ta reda på vem som åt kakan beroende på datatyp.
+
+
+//b. // INPUTS: Vilka variabler som kommer till funktionen och vilken typ de har
+
+// String, number.
+
+
+//c. // RETURN: Vad ska skickas tillbaka efter beräkning eller behandling av
+//inputs och vilken typ har den – tex. result: decimaltal.
+
+// Namnet på den som åt kakan i en textinterpolering.
+
+
+//d. // STEG 1: Beskriv nu i tur och ordning (STEG 2, osv.)
+
+// Om input är en sträng, placera Zach i den tomma variablen "name" och returnera Zach åt kakan. 
+// Om input är ett nummer, placera Monica i den tomma variablen "name" och returnera Monica åt kakan.
+// Om input är något annat, placera "the dog" i den tomma variablen "name" och returnera hunden åt kakan.
+
+function cookie(x){
+
+  let name = "";
+  
+  if(typeof x === "string") {
+    name += "Zach";
+  } else if (typeof x === "number") {
+    name += "Monica";
+  } else {
+    name += "the dog";
+  }
+  
+return `Who ate the last cookie? It was ${name}!`
+}
