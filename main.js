9903c1ededc3f07c1e4896ceb08fc9d9d50051f5
meskilsson <mattiaseskilsson@hotmail.se>
2025-09-24 12:12:22 +0200

    reversed words

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-//a. //MÅL:Ta reda på vem som åt kakan beroende på datatyp.
+//MÅL:
+// Ta reda på vem som åt kakan beroende på datatyp.
+
+// INPUTS: Vilka variabler som kommer till funktionen och vilken typ de har:
+
+// String, number(integer).
 
 
-//b. // INPUTS: Vilka variabler som kommer till funktionen och vilken typ de har
-
-// String, number.
-
-
-//c. // RETURN: Vad ska skickas tillbaka efter beräkning eller behandling av
-//inputs och vilken typ har den – tex. result: decimaltal.
+// RETURN: Vad ska skickas tillbaka efter beräkning eller behandling av inputs och vilken typ har den – tex. result: decimaltal.
 
 // Namnet på den som åt kakan i en textinterpolering.
 
 
-//d. // STEG 1: Beskriv nu i tur och ordning (STEG 2, osv.)
+// STEG 1: Beskriv nu i tur och ordning (STEG 2, osv.)
 
 // Om input är en sträng, placera Zach i den tomma variablen "name" och returnera Zach åt kakan. 
 // Om input är ett nummer, placera Monica i den tomma variablen "name" och returnera Monica åt kakan.
 // Om input är något annat, placera "the dog" i den tomma variablen "name" och returnera hunden åt kakan.
+
+
 
 function cookie(x){
 
@@ -29,6 +30,6 @@ function cookie(x){
   } else {
     name += "the dog";
   }
-  
+
 return `Who ate the last cookie? It was ${name}!`
-}
+} 
---
+++ two.js
@@ -0,0 +1,16 @@
+// REVERSED WORDS
+
+//MÅL
+//Omvandla ordningen så att de kommer bak och fram.
+
+//INPUT
+//En sträng med flera ord.
+
+//RETURN
+//Samma sträng i omvänd ordning.
+
+//FUNKTIONSBESKRIVNING
+// Dela upp strängen i en array med enskilda ord.
+// vänd på ordningen i arrayen.
+// Sätt ihop arrayen till en sträng igen med mellanslag mellan orden.
+// Returnera den nya strängen.
